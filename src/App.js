4d2269f033bb5b81d00e85039de7c38eab9b1995
murtaza-yazilim2 <yazilim2@birfatura.com>
2021-07-20 16:24:29 +0300

    End of Section-3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses.js';
 
-function App() {
-  const expenses = [
+const App = () => {
+
+const expenses = [
     {title:'Car Insurence' , amount : '123' , date: new Date(2021,3,28)},
     {title:'Test Deneme' , amount : '345' , date: new Date(2021,5,18)},
     {title:'Falan filan' , amount : '1212' , date: new Date(2021,1,8)}
   ];
-
-
   return (
     <div>
       <h2>Lets get started!</h2>
-  
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-
-      
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
